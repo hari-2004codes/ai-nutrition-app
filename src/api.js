5d2975ed69edfa137303857e4af59089cf00d575
Mahalakshmi Karikalan <126969087+mahalakshmi-karikalan@users.noreply.github.com>
2025-07-01 18:44:22 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://NutriTracker-backend.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL || "https://nutritracker-backend.onrender.com/api",
   headers: {
     "Content-Type": "application/json"
   }
